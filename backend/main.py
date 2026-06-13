c4f7d00089176c29eb36422090caca2df84ac750
Sophia Thomas <sophiart03@gmail.com>
2026-06-13 13:16:32 -0700

    refactor: backend/frontend

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,3 @@
-from router import get_json
-import json 
 from fastapi import FastAPI
 from router import project_router, experience_router, company_router, bullet_router, education_router
 
@@ -15,6 +13,3 @@ app.include_router(education_router)
 @app.get("/")
 def read_root(): 
     return {"message": "Welcome to my portfolio API. Head to /docs for the swagger UI."}
-
-# pretty_json = json.dumps(get_json(), indent=4)
-# print(pretty_json)
